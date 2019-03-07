f6619866e06163113f94f8797d79fb1ccb2ab5e7
ananya-kundu <ananya.kundu@accenture.com>
2019-03-07 16:25:50 +0530

    scrollbar fixing

--- lps/src/app/Site/site-tabs/site-dynamic-tab.component.ts
+++ lps/src/app/Site/site-tabs/site-dynamic-tab.component.ts
@@ -112,6 +112,7 @@ export class SiteDynamicTabComponent implements OnInit, AfterViewInit, OnDestroy
         // this.sectionUrl = `http://localhost:12231/LeaseMoreUIService/lease/details`;
       }else if(response == 'notes'){
         this.sectionUrl = environment.leaseMoreInfo+'/LeaseMoreUIService/lease/Notes';
+        // this.sectionUrl = `http://localhost:12231/LeaseMoreUIService/lease/Notes`;
       }else if(response == 'mla'){
         this.sectionUrl = environment.leaseMoreInfo+'/LeaseMoreUIService/lease/mla';
       }else if(response == 'contractManager'){
